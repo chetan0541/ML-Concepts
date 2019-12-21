8bae11649a1fe55cabbc4833524ba495086ee1b6
chetan0541 <33390795+chetan0541@users.noreply.github.com>
2019-12-21 19:19:05 +0100

    Imputer
    Dealt with missing data using Imputer

---
+++ Missing_Data_Imputer.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+from sklearn.preprocessing import Imputer #depriciated 
+from sklearn.impute import SimpleImputer #new
+
+
+data = pd.read_csv("Data.csv")
+
+X = data.iloc[:,:-1].values
+y = data.iloc[:,3]
+
+# print("y:",y)
+
+# print("X 1:2", X[:,1:2])
+# print("X 1:3", X[:,1:3])
+Imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+
+Imputer_X = Imputer.fit(X[:,1:3])
+X[:,1:3] = Imputer_X.transform(X[:,1:3])
+
+print("X:",X)
